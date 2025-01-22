abbf924c56a4e555d3de7a6cdd270683b405ca8e
rpawlinski <radoslaw.pawlinski@gmail.com>
2025-01-22 13:50:30 +0100

    dodanie name do konstruktora

--- lesson_04.py
+++ lesson_04.py
@@ -1,14 +1,15 @@
 class OrcRace:
     battle_cry = "Za hordę!"
 
-    def __init__(self, damage, health, cost):
+    def __init__(self, name, damage, health, cost):
+        self.name = name
         self.damage = damage
         self.health = health
         self.cost_of_production = cost
 
     def attack(self):
         print(OrcRace.battle_cry)
-        print(f"Zadałeś {self.damage} obrażeń!")
+        print(f"{self.name} zadał {self.damage} obrażeń!")
 
     def go_to(self, x, y):
         print(f"Idziesz do współrzędnych: {x}, {y}")
