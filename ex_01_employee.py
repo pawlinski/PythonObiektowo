fc61259eb546be39c47f2476abbdb4069747110e
rpawlinski <radoslaw.pawlinski@gmail.com>
2025-01-22 14:41:43 +0100

    zadanie: klasa Employee

---
+++ ex_01_employee.py
@@ -0,0 +1,35 @@
+class Employee:
+    count = 0
+
+    def __init__(self, name, surename, email, salary):
+        self.name = name
+        self.surename = surename
+        self.email = email
+        self.monthly_salary = salary
+
+    # metoda oblicza i zwraca wynagrodzenie roczne pracownika
+    def get_annual_salary(self):
+        return self.monthly_salary * 12
+
+    # metoda pokazuje w konsoli podstawowe informacje o pracowniku
+    def show_employee_information(self):
+        print(f"Pracownik: {self.name} {self.surename}, adres e-mail: {self.email}, wynagrodzenie miesięczne: {self.monthly_salary}")
+
+def main():
+    employee_01 = Employee("Maciej", "Kowalski", "m.kowalski@gmail.com", 4800)
+    employee_02 = Employee("Zofia", "Mała", "z.mala@poczta.fm", 12300)
+    employee_03 = Employee("Kacper", "Ruciński", "k.rut@o2.pl", 8700)
+
+    list_of_employee = []
+    list_of_employee.append(employee_01)
+    list_of_employee.append(employee_02)
+    list_of_employee.append(employee_03)
+
+    for employee in list_of_employee:
+        employee.count += 1 # zlicza ilość pracowników
+        print(f"{employee.name} {employee.surename} zarabia rocznie {employee.get_annual_salary()} zł.")
+        employee.show_employee_information()
+
+    print(f"Nasza firma ma zatrudnionych {Employee.count} pracowników.")
+
+main()
